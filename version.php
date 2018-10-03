1766cdfb750f0b45de0744370ebcc02f769119eb
David Thompson <david.thompson@catalyst.net.nz>
2018-10-04 09:38:04 +1300

    Change required Moodle version.

--- version.php
+++ version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018090300;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2018051701;        // Requires this Moodle version
+$plugin->requires  = 2015051110;        // Requires this Moodle version.
 $plugin->component = 'block_coursecollection';
